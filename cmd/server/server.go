e7c260642fa37c1c6ebdc6d2b6cddbeabd295119
Jubaer Hossain <jubaer01.cse@gmail.com>
2024-02-18 10:05:47 +0600

    commit

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -1,15 +1,9 @@
 package main
 
 import (
-    "context"
-    "fmt"
-    "log"
-    "time"
+	"log"
 
-    "google.golang.org/grpc"
-	"google.golang.org/grpc/credentials"
-    pb "github.com/JubaerHossain/golang-ddd/internal/infrastructure/grpc" // Import the generated gRPC client package
-
+	"github.com/JubaerHossain/golang-ddd/internal/core/server" // Import the generated gRPC client package
 )
 
 const (
@@ -17,24 +11,8 @@ const (
 )
 
 func main() {
-	// Set up transport credentials for insecure connection
-	creds := credentials.NewInsecure()
-
-	// Set up a connection to the server with transport credentials
-	conn, err := grpc.Dial(address, grpc.WithTransportCredentials(creds))
+	err := server.Start()
 	if err != nil {
-		log.Fatalf("did not connect: %v", err)
+		log.Fatalf("failed to start server: %v", err)
 	}
-	defer conn.Close()
-	client := pb.NewFoodAppClient(conn)
-
-	// Example call to the gRPC server
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	defer cancel()
-	response, err := client.SomeRPCMethod(ctx, &pb.SomeRequest{ /* Provide request data */ })
-	if err != nil {
-		log.Fatalf("Error calling gRPC method: %v", err)
-	}
-	// Process the response from the server
-	fmt.Println("Response:", response)
 }
