e8f7c69d8ec2015975e2bc1615f726ce24933b10
Jubaer Hossain <jubaer01.cse@gmail.com>
2024-02-19 00:25:35 +0600

    commit

--- internal/user/domain/entity/user.go
+++ internal/user/domain/entity/user.go
@@ -28,7 +28,7 @@ const (
 
 // User represents the user entity
 type User struct {
-	ID        uint   `json:"id"`
+	ID        uint   `json:"id" gorm:"primaryKey;autoIncrement;not null"` // Primary key
 	Username  string `json:"username" validate:"required,min=3,max=50"`
 	Email     string `json:"email" validate:"required,email,max=100"`
 	Password  string `json:"password" validate:"required,min=6,max=20"`
