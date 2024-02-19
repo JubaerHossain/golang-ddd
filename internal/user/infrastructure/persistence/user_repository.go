76a9866d90703c07613249a43cb44817e8cecfd0
Jubaer Hossain <jubaer01.cse@gmail.com>
2024-02-19 22:49:23 +0600

    commit

--- internal/user/infrastructure/persistence/user_repository.go
+++ internal/user/infrastructure/persistence/user_repository.go
@@ -162,7 +162,7 @@ func (r *UserRepositoryImpl) Login(loginUser *entity.LoginUser) (*entity.LoginUs
 		return nil, fmt.Errorf("user not found")
 	}
 	if err := utilQuery.ComparePassword(user.Password, loginUser.Password); err != nil {
-		return nil,  err
+		return nil,  fmt.Errorf("invalid password")
 	}
 	token, err := auth.CreateToken(user)
 	if err != nil {
